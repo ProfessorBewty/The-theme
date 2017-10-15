c14905fe95068d832f6877f003a21a9167aa7ce9
ProfessorBewty <32783420+ProfessorBewty@users.noreply.github.com>
2017-10-14 17:06:34 -0700

    Update functions.php
    changed file directory

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function first_script_enqueue(){
-	wp_enqueue_style('customstyle', /var/www/html/wordpress/wp-content/themes/practice-theme, array(), '1.0.0', 'all');
+	wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/practice-theme.css', array(), '1.0.0', 'all');
 }
 
 add_action('wp_enqueue_scripts', 'first_script_enqueue');
