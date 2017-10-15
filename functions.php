8eb1e81aea7f112a9cc9e898c5f5fcd37473e93d
kennapea <32804348+kennapea@users.noreply.github.com>
2017-10-14 18:21:18 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-function first_script_enqueue(){
+function first_script_enqueue() {
 	wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/practice.css', array(), '1.0.0', 'all');
 	wp_enqueue_script('customjs', get_template_directory_uri() . '/js/practice.js', array(), '1.0.0', 'true');
 }
