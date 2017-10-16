bec322da4d26bd42723419d5338e98869ecceef7
ProfessorBewty <32783420+ProfessorBewty@users.noreply.github.com>
2017-10-16 16:40:55 -0700

    Update header.php

--- header.php
+++ header.php
@@ -9,3 +9,4 @@
 	<body>
 		
 		<?php wp_nav_menu(array('theme_location'=>'first')); ?>
+		<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custome_header()->width; ?>" alt"" /> 
